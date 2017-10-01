acebdeb9477a2d91fcb1dcffe2a1474ef0b102e7
Darwin Maturan <maturan72@gmail.com>
2017-10-01 13:29:48 +0930

    Barangay

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,3 +23,45 @@ function collapsenav(){
 };
 $(window).scroll(collapsenav);
 $(document).ready(collapsenav);
+
+
+
+// last section 
+
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
+  $('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+
+  $(".generalInformation .container .col-12 a").click(function(e){
+        e.preventDefault();
+        var id=$(this).attr('href');
+        $('.collapse').collapse('hide');
+        $(id).collapse('show');
+  })
+      
